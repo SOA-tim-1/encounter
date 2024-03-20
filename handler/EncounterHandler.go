283bb6c247d98b90279654c6dc5b475a282686d9
Breskva <vukmacanovic@gmail.com>
2024-03-20 10:52:45 +0100

    Implement EncounterHandler

---
+++ handler/EncounterHandler.go
@@ -0,0 +1,82 @@
+package handler
+
+import (
+	"database-example/dtos"
+	"database-example/service"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type EncounterHandler struct {
+	EncounterService service.IEncounterService
+}
+
+func (handler *EncounterHandler) GetAll(writer http.ResponseWriter, req *http.Request) {
+	encounters, err := handler.EncounterService.GetAll()
+	if err != nil {
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(encounters)
+}
+
+func (handler *EncounterHandler) Create(writer http.ResponseWriter, req *http.Request) {
+	var encounter dtos.EncounterDto
+	err := json.NewDecoder(req.Body).Decode(&encounter)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	createdEncounter, err := handler.EncounterService.Create(&encounter)
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusCreated)
+	json.NewEncoder(writer).Encode(createdEncounter)
+}
+
+func (handler *EncounterHandler) Update(writer http.ResponseWriter, req *http.Request) {
+	var encounter dtos.EncounterDto
+	err := json.NewDecoder(req.Body).Decode(&encounter)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	updatedEncounter, err := handler.EncounterService.Update(&encounter)
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(updatedEncounter)
+}
+
+func (handler *EncounterHandler) Delete(writer http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	encounterId, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = handler.EncounterService.Delete(encounterId)
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
+}
